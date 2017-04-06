4712cab46d6a644dda7975216ed62f95d1c3d11d
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-04-06 10:29:42 +0200

    Initial condo text

--- app/condo/src/Condo.php
+++ app/condo/src/Condo.php
@@ -2,6 +2,7 @@
 
 use Condo\Controller\Condo as CondoController;
 use Pckg\Framework\Provider;
+use Pckg\Generic\Provider\GenericPaths;
 
 class Condo extends Provider
 {
@@ -10,6 +11,13 @@ class Condo extends Provider
     {
         return [
             (new Pckg\Framework\Router\Route\Route('/', 'index', CondoController::class)),
+        ];
+    }
+
+    public function providers()
+    {
+        return [
+            GenericPaths::class,
         ];
     }
 
