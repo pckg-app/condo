136c790f628c558d499020761340ec2ae64ea6e0
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-08-05 21:56:29 +0200

    FIx

--- app/condo/src/Condo/Controller/Condo.php
+++ app/condo/src/Condo/Controller/Condo.php
@@ -48,32 +48,27 @@ class Condo
     {
         $repositoryUrl = post('repository.html_url', null) . '.git';
 
-        return;
-        $changes = post('push.changes', []);
-        foreach ($changes as $change) {
-            if ($change['new']['type'] != 'branch') {
-                continue;
-            }
-
-            $branch = $change['new']['name']; // master, preprod, develop, BR-asd, bla-bla, foo-bar, ...
-
-            /**
-             * This is first call that is made after git push, we will trigger all actions, wooow. :)
-             * First, check for repository in our database.
-             */
-            $repository = (new Repositories())->where('source', $repositoryUrl)->oneOr(function() use ($repositoryUrl) {
-                throw new Exception('Repository ' . $repositoryUrl . ' not found in Condo');
-            });
-
-            /**  Merge feature, bugfix and hotfix branch to preprod.
-             *      If successful, deploy to preprod
-             */
-            (new Branches())->where('repository_id', $repository->id)
-                            ->where('branch', $branch)
-                            ->oneAndIf(function(Branch $branch) {
-                                $branch->webhookActivated();
-                            });
+        $branch = str_replace('refs/heads/', '', post('ref', null));
+        if (!$branch) {
+            return;
         }
+
+        /**
+         * This is first call that is made after git push, we will trigger all actions, wooow. :)
+         * First, check for repository in our database.
+         */
+        $repository = (new Repositories())->where('source', $repositoryUrl)->oneOr(function() use ($repositoryUrl) {
+            throw new Exception('Repository ' . $repositoryUrl . ' not found in Condo');
+        });
+
+        /**  Merge feature, bugfix and hotfix branch to preprod.
+         *      If successful, deploy to preprod
+         */
+        (new Branches())->where('repository_id', $repository->id)
+                        ->where('branch', $branch)
+                        ->oneAndIf(function(Branch $branch) {
+                            $branch->webhookActivated();
+                        });
     }
 
     public function postWebhookAction()
