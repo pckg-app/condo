0d2c2921568312254b42fa6184f2ed7acc267641
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-08-05 21:54:12 +0200

    Provider split

--- app/condo/src/Condo/Controller/Condo.php
+++ app/condo/src/Condo/Controller/Condo.php
@@ -13,15 +13,9 @@ class Condo
         return view('index');
     }
 
-    public function postWebhookAction()
+    public function postBitbucketWebhookAction()
     {
-        $repositoryUrl = post('repository.links.html.href', null);
-
-        if (!$repositoryUrl) {
-            throw new Exception("Repository not set!");
-        }
-
-        $repositoryUrl .= '.git';
+        $repositoryUrl = post('repository.links.html.href', null) . '.git';
 
         $changes = post('push.changes', []);
         foreach ($changes as $change) {
@@ -47,6 +41,55 @@ class Condo
                             ->oneAndIf(function(Branch $branch) {
                                 $branch->webhookActivated();
                             });
+        }
+    }
+
+    public function postGithubWebhookAction()
+    {
+        $repositoryUrl = post('repository.html_url', null) . '.git';
+
+        return;
+        $changes = post('push.changes', []);
+        foreach ($changes as $change) {
+            if ($change['new']['type'] != 'branch') {
+                continue;
+            }
+
+            $branch = $change['new']['name']; // master, preprod, develop, BR-asd, bla-bla, foo-bar, ...
+
+            /**
+             * This is first call that is made after git push, we will trigger all actions, wooow. :)
+             * First, check for repository in our database.
+             */
+            $repository = (new Repositories())->where('source', $repositoryUrl)->oneOr(function() use ($repositoryUrl) {
+                throw new Exception('Repository ' . $repositoryUrl . ' not found in Condo');
+            });
+
+            /**  Merge feature, bugfix and hotfix branch to preprod.
+             *      If successful, deploy to preprod
+             */
+            (new Branches())->where('repository_id', $repository->id)
+                            ->where('branch', $branch)
+                            ->oneAndIf(function(Branch $branch) {
+                                $branch->webhookActivated();
+                            });
+        }
+    }
+
+    public function postWebhookAction()
+    {
+        $repositoryUrl = post('repository.links.html.href', null);
+        if ($repositoryUrl) {
+            $this->postBitbucketWebhookAction();
+        } else {
+            $repositoryUrl = post('repository.html_url', null);
+            if ($repositoryUrl) {
+                $this->postGithubWebhookAction();
+            }
+        }
+
+        if (!$repositoryUrl) {
+            throw new Exception("Repository not set!");
         }
 
         return 'ok';
