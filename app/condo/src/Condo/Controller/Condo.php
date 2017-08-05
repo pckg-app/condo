9b270b220dd08e932147068b704a86cb22dcbac0
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-08-05 21:47:08 +0200

    Condo

--- app/condo/src/Condo/Controller/Condo.php
+++ app/condo/src/Condo/Controller/Condo.php
@@ -15,25 +15,39 @@ class Condo
 
     public function postWebhookAction()
     {
-        $repositoryUrl = 'https://bitbucket.org/gnp/derive.git';
-        $branch = 'preprod'; // master, preprod, develop, BR-asd, bla-bla, foo-bar, ...
+        $repositoryUrl = post('repository.links.html.href', null);
 
-        /**
-         * This is first call that is made after git push, we will trigger all actions, wooow. :)
-         * First, check for repository in our database.
-         */
-        $repository = (new Repositories())->where('source', $repositoryUrl)->oneOr(function() use ($repositoryUrl) {
-            throw new Exception('Repository ' . $repositoryUrl . ' not found in Condo');
-        });
+        if (!$repositoryUrl) {
+            throw new Exception("Repository not set!");
+        }
 
-        /**  Merge feature, bugfix and hotfix branch to preprod.
-         *      If successful, deploy to preprod
-         */
-        (new Branches())->where('repository_id', $repository->id)
-                        ->where('branch', $branch)
-                        ->oneAndIf(function(Branch $branch) {
-                            $branch->webhookActivated();
-                        });
+        $repositoryUrl .= '.git';
+
+        $changes = post('push.changes', []);
+        foreach ($changes as $change) {
+            if ($change['new']['type'] != 'branch') {
+                continue;
+            }
+
+            $branch = $change['new']['name']; // master, preprod, develop, BR-asd, bla-bla, foo-bar, ...
+
+            /**
+             * This is first call that is made after git push, we will trigger all actions, wooow. :)
+             * First, check for repository in our database.
+             */
+            $repository = (new Repositories())->where('source', $repositoryUrl)->oneOr(function() use ($repositoryUrl) {
+                throw new Exception('Repository ' . $repositoryUrl . ' not found in Condo');
+            });
+
+            /**  Merge feature, bugfix and hotfix branch to preprod.
+             *      If successful, deploy to preprod
+             */
+            (new Branches())->where('repository_id', $repository->id)
+                            ->where('branch', $branch)
+                            ->oneAndIf(function(Branch $branch) {
+                                $branch->webhookActivated();
+                            });
+        }
 
         return 'ok';
     }
