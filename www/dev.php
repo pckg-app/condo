107cf40d97056615e48919ddaea73235f8a256dc
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-01-27 01:51:50 +0100

    Change

--- www/dev.php
+++ www/dev.php
@@ -3,5 +3,5 @@
 /**
  * Simply require development entry point, framework will take care of things.
  */
-define('__ROOT__', dirname(dirname(__FILE__)));
-require_once __ROOT__ . "/vendor/pckg/framework/src/development.php";
+define('__ROOT__', dirname(dirname(__FILE__)) . '/');
+require_once __ROOT__ . 'vendor/pckg/framework/src/development.php';
--- www/index.php
+++ www/index.php
@@ -3,5 +3,5 @@
 /**
  * Simply require production entry point, framework will take care of things.
  */
-define('__ROOT__', dirname(dirname(__FILE__)));
-require_once __ROOT__ . "/vendor/pckg/framework/src/production.php";
+define('__ROOT__', dirname(dirname(__FILE__)) . '/');
+require_once __ROOT__ . 'vendor/pckg/framework/src/production.php';
